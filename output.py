6d4b913220225c3f4a8fcb2c0d33bf5354951ebe
Shubham Kohli <96737853+shubhamkohli19@users.noreply.github.com>
2022-01-10 00:09:17 +0530

    Add files via upload

---
+++ output.py
@@ -0,0 +1,42 @@
+# Output-1
+
+Welcome. This is a program to find the average of three numbers. 
+Enter first number 12
+Enter second number 4
+Enter third number 8
+The average of three numbers is :
+8.0
+
+# Output-2
+
+Welcome. This is a program to find your Income Tax.
+Enter your gross Income(in $)50000
+Enter your number of dependents2
+Your Income Tax is :
+$7399.6
+
+# Output-3
+
+Please fill in the required details:
+Enter your SID 21103065
+Enter your name Shubham Kohli
+Enter your Gender: 'M','F','U'(for Unknown) M
+Enter your course name Computer Science
+Enter your CGPA 9.3
+
+# Output-4
+
+Enter marks of student 1 91
+Enter marks of student 2 88
+Enter marks of student 3 89
+Enter marks of student 4 76
+Enter marks of student 5 92
+['76', '88', '89', '91', '92']
+
+# Output-5
+
+Solution of part (a) of this question is:
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+
+Solution of part (b) of this question is:
+['Red', 'Green', 'White', 'Purple', 'Yellow']
