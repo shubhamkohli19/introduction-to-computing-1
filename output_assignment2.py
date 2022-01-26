69a534dbf94b6734f9696e041307ed40e7c309b8
Shubham Kohli <96737853+shubhamkohli19@users.noreply.github.com>
2022-01-26 10:05:08 +0530

    Add files via upload

---
+++ output_assignment2.py
@@ -0,0 +1,53 @@
+# Output of Question-1
+
+We have given the string 'Python is a case sensitive language'
+The Length of the string is 35
+The Reversed order of the string is 'egaugnal evitisnes esac a si nohtyP'
+a case sensitive
+Python is object oriented language
+The index of substring 'a' in the given string is 10
+The given string without any whitespaces: 'Pythonisacasesensitivelanguage'
+
+# Output of Question-2
+
+Welcome to this program
+Please Enter your details 
+Enter your name Shubham Kohli
+Enter your Student ID number 21103065
+Enter your Department of Study Computer Science
+Hey, Shubham Kohli Here!
+My SID is 21103065
+I am from Computer Science department and my CGPA is 9.9
+
+# Output of Question-3
+
+This is a program to Calculate any two integers through Bitwise Operators.
+We have a = 56 and b = 10
+Now, using Bitwise Operators.
+a&b =  8
+a|b =  58
+a^b =  50
+Left shift both a and b with 2 bits gives 224 and 40 respectively.
+Right shift 'a' with 2 bits and 'b' with 4 bits gives 14 and 0 respectively.
+
+# Output of Question-4
+
+This is the program to find the greatest integer between any three numbers.
+Enter the first number 345
+Enter the second number 346
+Enter the third number 343
+The greatest integer between 345,346 and 343 is 346
+
+# Output of Question-5
+
+This is the program to find out if your program has the word 'name' in it
+Enter a String Mynameisshubhamkohli
+Yes
+
+# Output of Question-6
+
+This is the program to check if you can make a triangle with three input side values.
+ENTER FIRST SIDE OF THE TRIANGLE 35
+ENTER SECOND SIDE OF THE TRIANGLE 15
+ENTER THIRD SIDE OF THE TRIANGLE 20
+No
