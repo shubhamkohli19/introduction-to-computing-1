f11b0f85f25e54e0a9d0b4093eab6ee9419cec41
Shubham Kohli <96737853+shubhamkohli19@users.noreply.github.com>
2022-03-02 18:02:11 +0530

    Add files via upload

---
+++ Output_assignment4.py
@@ -0,0 +1,97 @@
+# Output-1
+
+Enter the number of disks: 3
+Move disk 1 from rod A to rod C
+Move disk 2 from rod A to rod B
+Move disk 1 from rod C to rod B
+Move disk 3 from rod A to rod C
+Move disk 1 from rod B to rod A
+Move disk 2 from rod B to rod C
+Move disk 1 from rod A to rod C
+
+
+# Output-2
+
+Enter the number: 5
+      1 
+     1 1 
+    1 2 1 
+   1 3 3 1 
+  1 4 6 4 1 
+
+
+#Output-3
+
+Enter first integer: 3
+Enter second integer: 8
+
+(Quotient, Remainder) is (0, 3)
+
+PART A Function is callable or not:  False
+
+PART B
+One/more digit is zero.
+
+PART C
+List after adding new values is [0, 3, 4, 5, 6]
+List after filtering out values greater than 4 is [0, 3, 4]
+
+PART D: convert to set
+{0, 3, 4}
+
+PART E: make set immutable
+frozenset({0, 3, 4})
+
+PART D: find max value and find its hash
+The maximum value from the set is  4
+Hash value of the max value is  4
+
+
+# Output-4
+
+Making an object...
+Student Class created.
+The name of the student is Sample_Name and Roll Number is 123
+Function end...
+Program ended.
+
+
+# Output-5
+
+Details of Employees:
+1 Mehak 40000
+2 Ashok 50000
+3 Viren 60000
+------
+
+PART A
+1 Mehak 70000
+
+PART B - refer to code as no output had to be printed for this part.
+
+
+# Output-6
+
+Enter the word from friend 1:elephant
+Enter the word from friend 2:tape
+ :( 
+
+ :( 
+
+ :)
+
+ :( 
+
+ :)
+
+ :( 
+
+ :)
+
+
+Press N if any of the above result was :( , otherwise press Y  ---- y
+
+
+Does the word 2 make sense? Y/n ---- y
+Friendship is true
+
